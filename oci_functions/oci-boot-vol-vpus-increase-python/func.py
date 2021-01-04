497f81aadbcb5053cd92f4b20481c0854f261091
revnav <mbpus052015@MBPUS052015s-MacBook-Pro-2.local>
2021-01-03 16:18:16 -0800

    fix for boot vol increase python

--- oci_functions/oci-boot-vol-vpus-increase-python/func.py
+++ oci_functions/oci-boot-vol-vpus-increase-python/func.py
@@ -17,13 +17,10 @@ def increase_bv_vpus (boot_vol_id):
     current_vpus = block_storage_client.get_boot_volume(boot_vol_id).data.vpus_per_gb
     print("INFO: current vpus for boot_vol {0}: {1}".format(boot_vol_id,current_vpus), flush=True)
     try:
-        if current_vpus != "20":
-            print('INFO: Updating boot_vol {}'.format(boot_vol_id), flush=True)
-            update_boot_volume_details = oci.core.models.UpdateBootVolumeDetails(vpus_per_gb=20)
-            resp = block_storage_client.update_boot_volume(boot_volume_id=boot_vol_id, update_boot_volume_details=update_boot_volume_details)
-            print(resp, flush=True)
-        else:
-            print("ERROR: Boot Vol already at higher performamnce, vpus for boot_vol {0}: {1}".format(boot_vol_id,current_vpus), flush=True)
+        print('INFO: Updating boot_vol {}'.format(boot_vol_id), flush=True)
+        update_boot_volume_details = oci.core.models.UpdateBootVolumeDetails(vpus_per_gb=20)
+        resp = block_storage_client.update_boot_volume(boot_volume_id=boot_vol_id, update_boot_volume_details=update_boot_volume_details)
+        print(resp, flush=True)
         except Exception as ex:
         print('ERROR: cannot update update_boot_volume {}'.format(boot_vol_id), flush=True)
         raise
@@ -38,6 +35,7 @@ def handler(ctx, data: io.BytesIO=None):
     except (Exception, ValueError) as ex:
         print(str(ex), flush=True)
 
+if current_vpus != "20":
     if alarm_msg["type"] == "OK_TO_FIRING":
         if alarm_msg["alarmMetaData"][0]["dimensions"]:
             alarm_metric_dimension = alarm_msg["alarmMetaData"][0]["dimensions"][0]   #assuming the first dimension matches the boot vol to resize
@@ -50,6 +48,9 @@ def handler(ctx, data: io.BytesIO=None):
     else:
         print('INFO: Nothing to do, alarm is not FIRING', flush=True)
         func_response = "Nothing to do, alarm is not FIRING"
+else:
+    print("INFO: Boot Vol already at higher performamnce, vpus for boot_vol {0}: {1}".format(boot_vol_id,current_vpus), flush=True)
+    func_response = "Boot Vol already at higher performamnce"
 
     return response.Response(
         ctx,
